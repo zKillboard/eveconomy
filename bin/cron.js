96ded3f2138640c731715375cb62d1542c1507b1
cvweiss <cvweiss@gmail.com>
2025-05-27 14:23:05 -0400

    testing

--- bin/cron.js
+++ bin/cron.js
@@ -1,1 +1,9 @@
+require('dotenv').config();
+
+const parsed = new URL(process.env.REDIS_URL);
+process.env.REDIS_HOST = parsed.hostname;
+process.env.REDIS_PORT = parsed.port;
+process.env.REDIS_AUTH = parsed.password;
+console.log(process.env);
+
 require('fundamen')('cron', {cron: process.argv[2]});
--- bin/www.js
+++ bin/www.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 const parsed = new URL(process.env.REDIS_URL);
-const process.env.REDIS_HOST = parsed.hostname;
-const process.env.REDIS_PORT = parsed.port;
-const process.env.REDIS_AUTH = parsed.password;
+process.env.REDIS_HOST = parsed.hostname;
+process.env.REDIS_PORT = parsed.port;
+process.env.REDIS_AUTH = parsed.password;
 console.log(process.env);
 
 require('fundamen')('www');
