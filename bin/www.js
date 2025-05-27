a6610271284db133988183db037456fa4ad31c72
cvweiss <cvweiss@gmail.com>
2025-05-27 14:20:06 -0400

    test

--- bin/www.js
+++ bin/www.js
@@ -1,1 +1,9 @@
+require('dotenv').config();
+
+const parsed = new URL(process.env.REDIS_URL);
+const process.env.REDIS_HOST = parsed.hostname;
+const process.env.REDIS_PORT = parsed.port;
+const process.env.REDIS_AUTH = parsed.password;
+console.log(process.env);
+
 require('fundamen')('www');
